badbc19bfed64b0e20b750c2367f0ee7db0766e0
NicoleGalindez <nbgalindez@misena.edu.co>
2023-10-16 20:41:52 -0500

    ejercicio rider

---
+++ reactgio_copia/miapp.js
@@ -0,0 +1,27 @@
+import express from "express";
+import cors from "cors";
+import router from "./rutas/mirutas.js";
+import mongoose from "mongoose";
+
+const app = express();
+
+app.use(cors()); // Enable CORS
+app.use(express.json()); // Parse JSON requests
+// Connect to MongoDB
+try {
+  await mongoose.connect('mongodb://localhost/estudiantes', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  console.log("MongoDB connection successful");
+} catch (error) {
+  console.error("Error connecting to MongoDB", error);}
+
+// Define routes
+app.use("/aprendices", router);
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
+
